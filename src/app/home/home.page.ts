f823936a0b83f13b3f078657d378c3c9cb8217ae
Bohdan <masliannykov@mail.ru>
2020-05-06 20:43:25 +0200

    repo build

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
+import { GitService } from '../shared/git.service';
+import { GitRepositories, Repository } from '../shared/git.model';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  repo: FormControl;
+  repos: Array<Repository> = [];
 
-  constructor() {}
+  constructor(private service: GitService) {}
 
+  ngOnInit() {
+    this.initNickControl();
+  }
+
+  initNickControl() {
+    this.repo = new FormControl();
+
+    this.repo.valueChanges
+      .pipe(debounceTime(50))
+      .subscribe((query: string) => this.service.getRepositories(query));
+  }
+
+  getRepositories(query: string) {
+    this.service.getRepositories(query).subscribe((repos: GitRepositories) => {
+      this.repos = repos.items;
+    });
+  }
+
+  searchRepos() {
+    this.getRepositories(this.repo.value);
+  }
 }
---
+++ src/app/shared/git.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { GitRepositories } from './git.model';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class GitService {
+  constructor(private http: HttpClient) {}
+
+  getRepositories(query: string): Observable<GitRepositories> {
+    const URL = `${environment.gitAPI}search/repositories`;
+    const params = { params: { q: query } };
+    return this.http.get(URL, params).pipe(map((d) => d as GitRepositories));
+  }
+}
